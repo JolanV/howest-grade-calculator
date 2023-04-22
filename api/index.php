7f99fe445ed0e4f4a789c0ec43682c713210c4dc
Angelo <angelo.hoevenaar@student.howest.be>
2023-04-22 11:49:11 +0200

    Add echo isset values to form

--- api/index.php
+++ api/index.php
@@ -10,73 +10,73 @@
 
 	<!-- Semester 1 -->
 		<label for="BIT">Basic IT skills:</label>
-		<input type="number" name="BIT" id="BIT">
-        <br>
+		<input type="number" name="BIT" id="BIT" value="<?php echo isset($_POST['BIT']) ? $_POST['BIT'] : ''; ?>">
+		<br>
 		<label for="CIB">Continues integration basics:</label>
-		<input type="number" name="CIB" id="CIB">
+		<input type="number" name="CIB" id="CIB" value="<?php echo isset($_POST['CIB']) ? $_POST['CIB'] : ''; ?>">
 		<br>
 		<label for="DB">Databases:</label>
-		<input type="number" name="DB" id="DB">
+		<input type="number" name="DB" id="DB" value="<?php echo isset($_POST['DB']) ? $_POST['DB'] : ''; ?>">
 		<br>
 		<label for="PRA">Programming Advanced:</label>
-		<input type="number" name="PRA" id="PRA" >
+		<input type="number" name="PRA" id="PRA" value="<?php echo isset($_POST['PRA']) ? $_POST['PRA'] : ''; ?>">
 		<br>
 		<label for="PRB">Programming Basics:</label>
-		<input type="number" name="PRB" id="PRB">
+		<input type="number" name="PRB" id="PRB" value="<?php echo isset($_POST['PRB']) ? $_POST['PRB'] : ''; ?>">
 		<br>
 		<label for="WFB">Web Frontend Basics:</label>
-		<input type="number" name="WFB" id="WFB">
+		<input type="number" name="WFB" id="WFB" value="<?php echo isset($_POST['WFB']) ? $_POST['WFB'] : ''; ?>">
 		<br>
 		<label for="WE">Worklplace Exploring</label>
-		<input type="number" name="WE" id="WE">
+		<input type="number" name="WE" id="WE" value="<?php echo isset($_POST['WE']) ? $_POST['WE'] : ''; ?>">
 		<br>
 
-	<!-- Semester 2 -->
+	 <!-- Semester 2 -->
 
-	    <label for="CIA">Continuous integration advanced:</label>
-		<input type="number" name="CIA" id="CIA">
-		<br>
-		<label for="PRE">Programming Expert:</label>
-		<input type="number" name="PRE" id="PRE">
-		<br>
-		<label for="WB">Web Backend:</label>
-		<input type="number" name="WB" id="WB">
-		<br>
-		<label for="WFA">Web Frontend Advanced:</label>
-		<input type="number" min = 0, max = 20 name="WFA" id="WFA">
-		<br>
-		<label for="WSI">Worklplace Simulation:</label>
-		<input type="number" name="WSI" id="WSI">
-		<br>
-
+	 	<label for="CIA">Continuous integration advanced:</label>
+        <input type="number" name="CIA" id="CIA" value="<?php echo isset($_POST['CIA']) ? $_POST['CIA'] : ''; ?>">
+        <br>
+        <label for="PRE">Programming Expert:</label>
+        <input type="number" name="PRE" id="PRE" value="<?php echo isset($_POST['PRE']) ? $_POST['PRE'] : ''; ?>">
+        <br>
+        <label for="WB">Web Backend:</label>
+        <input type="number" name="WB" id="WB" value="<?php echo isset($_POST['WB']) ? $_POST['WB'] : ''; ?>">
+        <br>
+        <label for="WFA">Web Frontend Advanced:</label>
+        <input type="number" min = 0, max = 20 name="WFA" id="WFA" value="<?php echo isset($_POST['WFA']) ? $_POST['WFA'] : ''; ?>">
+        <br>
+        <label for="WSI">Worklplace Simulation:</label>
+        <input type="number" name="WSI" id="WSI" value="<?php echo isset($_POST['WSI']) ? $_POST['WSI'] : ''; ?>">
+        <br>
 	<!-- Semester 3 -->
 
-	    <label for="AIT">Advanced IT skills:</label>
-		<input type="number" name="AIT" id="AIT">
-		<br>
-		<label for="MDE">Mobile Development:</label>
-		<input type="number" name="MDE" id="MDE">
-		<br>
-		<label for="PIN">Programming Innovation:</label>
-		<input type="number" name="PIN" id="PIN">
-		<br>
-		<label for="PRI">Programming Integration:</label>
-		<input type="number" name="PRI" id="PRI">
-		<br>
-		<label for="WE1">Workplace Experience 1:</label>
-		<input type="number" name="WE1" id="WE1">
-		<br>
+		<label for="AIT">Advanced IT skills:</label>
+        <input type="number" name="AIT" id="AIT" value="<?php echo isset($_POST['AIT']) ? $_POST['AIT'] : ''; ?>">
+        <br>
+        <label for="MDE">Mobile Development:</label>
+        <input type="number" name="MDE" id="MDE" value="<?php echo isset($_POST['MDE']) ? $_POST['MDE'] : ''; ?>">
+        <br>
+        <label for="PIN">Programming Innovation:</label>
+        <input type="number" name="PIN" id="PIN" value="<?php echo isset($_POST['PIN']) ? $_POST['PIN'] : ''; ?>">
+        <br>
+        <label for="PRI">Programming Integration:</label>
+        <input type="number" name="PRI" id="PRI" value="<?php echo isset($_POST['PRI']) ? $_POST['PRI'] : ''; ?>">
+        <br>
+        <label for="WE1">Workplace Experience 1:</label>
+        <input type="number" name="WE1" id="WE1" value="<?php echo isset($_POST['WE1']) ? $_POST['WE1'] : ''; ?>">
+        <br>
 
-	<!-- Semester 4 -->
+    <!-- Semester 4 -->
 
-	    <label for="IP">Innovative Project:</label>
-		<input type="number" name="IP" id="IP">
-		<br>
-		<label for="WE2">Workplace Experience 2:</label>
-		<input type="number" name="WE2" id="WE2">
-		<br>
+        <label for="IP">Innovative Project:</label>
+        <input type="number" name="IP" id="IP" value="<?php echo isset($_POST['IP']) ? $_POST['IP'] : ''; ?>">
+        <br>
+        <label for="WE2">Workplace Experience 2:</label>
+        <input type="number" name="WE2" id="WE2" value="<?php echo isset($_POST['WE2']) ? $_POST['WE2'] : ''; ?>">
+        <br>
 		
 		<input type="submit" name="submit" value="Calculate">
+		<input type="button" name="clear" value="Clear" onclick="clearForm()">
 
 	</form>
 	
@@ -149,6 +149,7 @@
 
 		}
 	?>
+
 	<script>
         function clearForm() {
             document.getElementById("BIT").value = "";
@@ -172,5 +173,6 @@
             document.getElementById("WE2").value = "";
         }
     </script>
+	
 </body>
 </html>
