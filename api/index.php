60b66dac0bc9def735a4f906559effedde4fbcd0
Angelo <angelo.hoevenaar@student.howest.be>
2023-04-22 11:55:38 +0200

    Add required tag and min and max value to input

--- api/index.php
+++ api/index.php
@@ -10,69 +10,69 @@
 
 	<!-- Semester 1 -->
 		<label for="BIT">Basic IT skills:</label>
-		<input type="number" name="BIT" id="BIT" value="<?php echo isset($_POST['BIT']) ? $_POST['BIT'] : ''; ?>">
+		<input type="number" name="BIT" id="BIT"  value="<?php echo isset($_POST['BIT']) ? $_POST['BIT'] : ''; ?>" min="0" max="20" required>
 		<br>
 		<label for="CIB">Continues integration basics:</label>
-		<input type="number" name="CIB" id="CIB" value="<?php echo isset($_POST['CIB']) ? $_POST['CIB'] : ''; ?>">
+		<input type="number" name="CIB" id="CIB" value="<?php echo isset($_POST['CIB']) ? $_POST['CIB'] : ''; ?>" min="0" max="20" required>
 		<br>
 		<label for="DB">Databases:</label>
-		<input type="number" name="DB" id="DB" value="<?php echo isset($_POST['DB']) ? $_POST['DB'] : ''; ?>">
+		<input type="number" name="DB" id="DB" value="<?php echo isset($_POST['DB']) ? $_POST['DB'] : ''; ?>" min="0" max="20" required>
 		<br>
 		<label for="PRA">Programming Advanced:</label>
-		<input type="number" name="PRA" id="PRA" value="<?php echo isset($_POST['PRA']) ? $_POST['PRA'] : ''; ?>">
+		<input type="number" name="PRA" id="PRA" value="<?php echo isset($_POST['PRA']) ? $_POST['PRA'] : ''; ?>" min="0" max="20" required>
 		<br>
 		<label for="PRB">Programming Basics:</label>
-		<input type="number" name="PRB" id="PRB" value="<?php echo isset($_POST['PRB']) ? $_POST['PRB'] : ''; ?>">
+		<input type="number" name="PRB" id="PRB" value="<?php echo isset($_POST['PRB']) ? $_POST['PRB'] : ''; ?>" min="0" max="20" required>
 		<br>
 		<label for="WFB">Web Frontend Basics:</label>
-		<input type="number" name="WFB" id="WFB" value="<?php echo isset($_POST['WFB']) ? $_POST['WFB'] : ''; ?>">
+		<input type="number" name="WFB" id="WFB" value="<?php echo isset($_POST['WFB']) ? $_POST['WFB'] : ''; ?>" min="0" max="20" required>
 		<br>
 		<label for="WE">Worklplace Exploring</label>
-		<input type="number" name="WE" id="WE" value="<?php echo isset($_POST['WE']) ? $_POST['WE'] : ''; ?>">
+		<input type="number" name="WE" id="WE" value="<?php echo isset($_POST['WE']) ? $_POST['WE'] : ''; ?>" min="0" max="20" required>
 		<br>
 
 	 <!-- Semester 2 -->
 
 	 	<label for="CIA">Continuous integration advanced:</label>
-        <input type="number" name="CIA" id="CIA" value="<?php echo isset($_POST['CIA']) ? $_POST['CIA'] : ''; ?>">
+        <input type="number" name="CIA" id="CIA" value="<?php echo isset($_POST['CIA']) ? $_POST['CIA'] : ''; ?>" min="0" max="20" required>
         <br>
         <label for="PRE">Programming Expert:</label>
-        <input type="number" name="PRE" id="PRE" value="<?php echo isset($_POST['PRE']) ? $_POST['PRE'] : ''; ?>">
+        <input type="number" name="PRE" id="PRE" value="<?php echo isset($_POST['PRE']) ? $_POST['PRE'] : ''; ?>" min="0" max="20" required> 
         <br>
         <label for="WB">Web Backend:</label>
-        <input type="number" name="WB" id="WB" value="<?php echo isset($_POST['WB']) ? $_POST['WB'] : ''; ?>">
+        <input type="number" name="WB" id="WB" value="<?php echo isset($_POST['WB']) ? $_POST['WB'] : ''; ?>" min="0" max="20" required>
         <br>
         <label for="WFA">Web Frontend Advanced:</label>
-        <input type="number" min = 0, max = 20 name="WFA" id="WFA" value="<?php echo isset($_POST['WFA']) ? $_POST['WFA'] : ''; ?>">
+        <input type="number" min = 0, max = 20 name="WFA" id="WFA" value="<?php echo isset($_POST['WFA']) ? $_POST['WFA'] : ''; ?>" min="0" max="20" required>
         <br>
         <label for="WSI">Worklplace Simulation:</label>
-        <input type="number" name="WSI" id="WSI" value="<?php echo isset($_POST['WSI']) ? $_POST['WSI'] : ''; ?>">
+        <input type="number" name="WSI" id="WSI" value="<?php echo isset($_POST['WSI']) ? $_POST['WSI'] : ''; ?>" min="0" max="20" required>
         <br>
 	<!-- Semester 3 -->
 
 		<label for="AIT">Advanced IT skills:</label>
-        <input type="number" name="AIT" id="AIT" value="<?php echo isset($_POST['AIT']) ? $_POST['AIT'] : ''; ?>">
+        <input type="number" name="AIT" id="AIT" value="<?php echo isset($_POST['AIT']) ? $_POST['AIT'] : ''; ?>" min="0" max="20" required>
         <br>
         <label for="MDE">Mobile Development:</label>
-        <input type="number" name="MDE" id="MDE" value="<?php echo isset($_POST['MDE']) ? $_POST['MDE'] : ''; ?>">
+        <input type="number" name="MDE" id="MDE" value="<?php echo isset($_POST['MDE']) ? $_POST['MDE'] : ''; ?>" min="0" max="20" required>
         <br>
         <label for="PIN">Programming Innovation:</label>
-        <input type="number" name="PIN" id="PIN" value="<?php echo isset($_POST['PIN']) ? $_POST['PIN'] : ''; ?>">
+        <input type="number" name="PIN" id="PIN" value="<?php echo isset($_POST['PIN']) ? $_POST['PIN'] : ''; ?>" min="0" max="20" required>
         <br>
         <label for="PRI">Programming Integration:</label>
-        <input type="number" name="PRI" id="PRI" value="<?php echo isset($_POST['PRI']) ? $_POST['PRI'] : ''; ?>">
+        <input type="number" name="PRI" id="PRI" value="<?php echo isset($_POST['PRI']) ? $_POST['PRI'] : ''; ?>" min="0" max="20" required>
         <br>
         <label for="WE1">Workplace Experience 1:</label>
-        <input type="number" name="WE1" id="WE1" value="<?php echo isset($_POST['WE1']) ? $_POST['WE1'] : ''; ?>">
+        <input type="number" name="WE1" id="WE1" value="<?php echo isset($_POST['WE1']) ? $_POST['WE1'] : ''; ?>" min="0" max="20" required>
         <br>
 
     <!-- Semester 4 -->
 
         <label for="IP">Innovative Project:</label>
-        <input type="number" name="IP" id="IP" value="<?php echo isset($_POST['IP']) ? $_POST['IP'] : ''; ?>">
+        <input type="number" name="IP" id="IP" value="<?php echo isset($_POST['IP']) ? $_POST['IP'] : ''; ?>" min="0" max="20" required>
         <br>
         <label for="WE2">Workplace Experience 2:</label>
-        <input type="number" name="WE2" id="WE2" value="<?php echo isset($_POST['WE2']) ? $_POST['WE2'] : ''; ?>">
+        <input type="number" name="WE2" id="WE2" value="<?php echo isset($_POST['WE2']) ? $_POST['WE2'] : ''; ?>" min="0" max="20" required>
         <br>
 		
 		<input type="submit" name="submit" value="Calculate">
