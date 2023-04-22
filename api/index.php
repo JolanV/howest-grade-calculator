b90a43d8380ebc13bdc548ffe9be9f0daeabf582
Angelo <angelo.hoevenaar@student.howest.be>
2023-04-22 14:28:27 +0200

    Fix bugs

--- api/index.php
+++ api/index.php
@@ -86,71 +86,67 @@
         
         /*Gets the grade values from the form*/
 		$grades = [
-			$bit => $_POST['BIT'],
-			$cib => $_POST['CIB'],
-			$db => $_POST['DB'],
-			$pra => $_POST['PRA'],
-			$prb => $_POST['PRB'],
-			$wfb => $_POST['WFB'],
-			$we => $_POST['WE'],
+			'bit' => $_POST['BIT'],
+			'cib' => $_POST['CIB'],
+			'db' => $_POST['DB'],
+			'pra' => $_POST['PRA'],
+			'prb' => $_POST['PRB'],
+			'wfb' => $_POST['WFB'],
+			'we' => $_POST['WE'],
+			'cia' => $_POST['CIA'],
+			'pre' => $_POST['PRE'],
+			'wb' => $_POST['WB'],
+			'wfa' => $_POST['WFA'],
+			'wsi' => $_POST['WSI'],
+			'ait' => $_POST['AIT'],
+			'mde' => $_POST['MDE'],
+			'pin' => $_POST['PIN'],
+			'pri' => $_POST['PRI'],
+			'we1' => $_POST['WE1'],
+			'ip' => $_POST['IP'],
+			'we2' => $_POST['WE2'],
+		];
 	
-			$cia => $_POST['CIA'],
-			$pre => $_POST['PRE'],
-			$wb => $_POST['WB'],
-			$wfa => $_POST['WFA'],
-			$wsi => $_POST['WSI'],
-	
-			$ait => $_POST['AIT'],
-			$mde => $_POST['MDE'],
-			$pin => $_POST['PIN'],
-			$pri => $_POST['PRI'],
-			$we1 => $_POST['WE1'],
-	
-			$ip => $_POST['IP'],
-			$we2 => $_POST['WE2'],
-
-		]
 		$study_weight = [
-			$bit => 3,
-			$cib => 3,
-			$db => 6,
-			$pra  => 5,
-			$prb => 6,
-			$wfb => 4,
-			$we => 3,
-			$cia => 3,
-			$pre => 6,
-			$wb => 9,
-			$wfa => 6,
-			$wsi => 6,
-			$ait => 3,
-			$mde => 6,
-			$pin => 9,
-			$pri => 6,
-			$we1 => 6,
-			$ip => 9,
-			$we2 => 21
-		]
-		
+			'bit' => 3,
+			'cib' => 3,
+			'db' => 6,
+			'pra' => 5,
+			'prb' => 6,
+			'wfb' => 4,
+			'we' => 3,
+			'cia' => 3,
+			'pre' => 6,
+			'wb' => 9,
+			'wfa' => 6,
+			'wsi' => 6,
+			'ait' => 3,
+			'mde' => 6,
+			'pin' => 9,
+			'pri' => 6,
+			'we1' => 6,
+			'ip' => 9,
+			'we2' => 21,
+		];
 		
 		/* functions */
         /* Multiply the grades with the study_weight */
 
-		public int multiply($grade, $study_weight) {
+		function multiply($grade, $study_weight)
+		{
 			return $grade * $study_weight;
 		}
-		/* functions end */
-
-		//creates sum of all grades and divides by 120
-		$scores = (array.map('multiply', $grades, $study_weight))/120;
-
-			echo "<p>Your grade will be: $scores</p>";
-
+	
+		$scores = array_map('multiply', $grades, $study_weight);
+		$final_grade = array_sum($scores) / 120;
+	
+        echo "<p id='echo'>Your grade will be: $final_grade</p>";
 		}
 	?>
 
 	<script>
         function clearForm() {
+			document.getElementById("echo").innerHTML = "";
             document.getElementById("BIT").value = "";
             document.getElementById("CIB").value = "";
             document.getElementById("DB").value = "";
