cfecbdf839b6d0b6998af1a03bc6f7b9f90899c0
Jolan Verstraete <91055383+JolanV@users.noreply.github.com>
2023-04-24 20:32:39 +0200

    Trying something crazy

--- api/index.php
+++ api/index.php
@@ -93,6 +93,14 @@
 		<input type="button" name="clear" value="Clear" onclick="clearForm()">
 	</form>
 	<?php
-    echo $result; ?>
+    echo $result;
+    
+    if ($_GET['type'] === 'css') {
+        header("Content-type: text/css; charset: UTF-8");
+        echo require __DIR__ . '/../public/css/' . basename($_GET['file']);
+    } else if ($_GET['type'] === 'js') {
+        header('Content-Type: application/javascript; charset: UTF-8');
+        echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
+    }?>
 </body>
 </html>
