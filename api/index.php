d7c14eefb0dc641bde07b7290725184dba4133c3
Jolan Verstraete <91055383+JolanV@users.noreply.github.com>
2023-04-24 20:39:25 +0200

    Revert Angelo's method

--- api/index.php
+++ api/index.php
@@ -94,13 +94,6 @@
 	</form>
 	<?php
     echo $result;
-    
-    if ($_GET['type'] === 'css') {
-        header("Content-type: text/css; charset: UTF-8");
-        echo require __DIR__ . '/../public/css/' . basename($_GET['file']);
-    } else if ($_GET['type'] === 'js') {
-        header('Content-Type: application/javascript; charset: UTF-8');
-        echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
-    }?>
+    ?>
 </body>
 </html>
